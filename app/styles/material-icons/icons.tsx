629e7007d2666ba8460d928d7a87c1586ba89d98
alexsmagalhaes <alexmagalhaesprofissional@gmail.com>
2026-02-10 08:05:36 -0300

    feat: add base icons components

---
+++ app/styles/material-icons/icons.tsx
@@ -0,0 +1,8 @@
+export function Icons() {
+  return (
+    <link
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,300,0,0&icon_names=add_2&display=swap"
+      rel="stylesheet"
+    />
+  );
+}
